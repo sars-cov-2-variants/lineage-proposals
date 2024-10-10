aafcb2a10822f4680835c59400983d2296ea1f46
aviczhl2 <zoux18@mails.tsinghua.edu.cn>
2024-10-10 18:04:33 +0800

    remove_past_recombs_and_add_bunchs_of_new

--- remove_by_issue.py
+++ remove_by_issue.py
@@ -1,6 +1,6 @@
 
 
-def remove_by_issue(issue_name,branch=None):
+def remove_by_issue(issue_name,branch=None,mode=None):
     q=open("recombinants.tsv",'r')
     existing_lines=q.readlines()
     q.close()
@@ -15,12 +15,21 @@ def remove_by_issue(issue_name,branch=None):
             to_remove=True
         else:
             issue_seq=linsp[1]
+            try:
+                issue_num=int(issue_seq.strip()[1:])
+            except:
+                issue_num=1000000
+            issue_link=linsp[-1]
             if str(issue_name) in issue_seq:
                 to_remove=True
                 if branch is not None:
                     to_remove=False
                     branch_seq=linsp[2]
                     if str(branch)==branch_seq:
+                        to_remove=True
+            if mode=='before':
+                if issue_num<=issue_name:
+                    if 'lineage-proposals' in issue_link:
                         to_remove=True
         if not(to_remove):
             new_lines.append(existing_lines[id])
@@ -33,9 +42,10 @@ def remove_by_issue(issue_name,branch=None):
 
 import argparse
 parser = argparse.ArgumentParser(description='Demo of argparse')
-parser.add_argument('--issue', type=int)
+parser.add_argument('--issue', type=int,default=0)
+parser.add_argument('--mode', type=str,default=None)
 parser.add_argument('--branch', type=int,default=None)
 
 args = parser.parse_args()
 
-w=remove_by_issue(args.issue,args.branch)
+w=remove_by_issue(args.issue,args.branch,args.mode)
